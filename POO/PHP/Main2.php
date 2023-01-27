fcde775c2d8fae636f56aea7061ae48a9a45f618
cristinAlvestegui <quitina.23@gmail.com>
2023-01-27 20:11:28 -0300

    namespace e use

--- POO/PHP/Main2.php
+++ POO/PHP/Main2.php
@@ -4,6 +4,7 @@
     require_once('Funcionario.php');
     require_once('contas.php');
 
-    $endere = new Address("Rua Bolivia", "09851760", "Casa 2", "São Paulo", "Neves", "SBC", "Brazil", "SP");
-    $client = new Cliente(124569878422, "Cristina", "119554648");
+    $endereCris = new Address("Rua Bolivia", "09851760", "Casa 2", "São Paulo", "Neves", "SBC", "Brazil", "SP");
+    $clientCris = new Cliente(124569878422, "Cristina", "119554648", $endereCris, 15);
+    $contaCris = new Conta($clientCris, 256546);
 ?>
